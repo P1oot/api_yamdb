b0e7d2bc1414fd2038475f09c2a0f42aaa4d4c89
Даниил Бердинских <dan.berdinskikh@mail.ru>
2022-08-23 23:29:41 +0300

    Change User model

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -3,7 +3,18 @@ from django.db import models
 
 
 class User(AbstractUser):
-    bio = models.TextField(
-        'Биография',
-        blank=True,
+    USER = 'user'
+    MODERATOR = 'moderator'
+    ADMIN = 'admin'
+    ROLE_CHOICES = [
+        (USER, 'User'),
+        (MODERATOR, 'Moderator'),
+        (ADMIN, 'Admin'),
+    ]
+    role = models.CharField(
+        max_length=10,
+        verbose_name='Роль',
+        help_text='Роль пользователя',
+        choices=ROLE_CHOICES,
+        default=USER,
     )
