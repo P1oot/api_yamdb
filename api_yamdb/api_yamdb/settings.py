47320df30e82b835186dcf25b6dfd3e683aba36e
Даниил Бердинских <dan.berdinskikh@mail.ru>
2022-08-23 16:56:54 +0300

    Add api app

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -21,7 +21,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users'
+    'rest_framework',
+    'users',
+    'api',
 ]
 
 MIDDLEWARE = [
